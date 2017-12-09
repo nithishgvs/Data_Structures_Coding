ad84cb220b8d1a5c063fa97a934d5b3a9e164528
Sai Nithish <sainithishgvs@gmail.com>
2017-12-09 14:58:37 -0500

    Dec 9 Push

---
+++ src/org/nithishgvs/linkedlist/SingleLLTest.java
@@ -0,0 +1,21 @@
+package org.nithishgvs.linkedlist;
+
+public class SingleLLTest {
+
+	public static void main(String[] args) {
+		SingleLLOperations<Integer> sll=new SingleLLOperations<>();
+		NodeSLL<Integer> headNode=sll.insertAtLast(3);
+		sll.insertAtLast(4);
+		sll.insertAtLast(5);
+		sll.insertAtLast(6);
+		sll.printSingleLL(headNode);
+		sll.deleteAtLast(headNode);
+		System.out.println();
+		sll.printSingleLL(headNode);
+		System.out.println();
+		sll.findAnElement(4);
+		sll.findAnElement(100);
+
+	}
+
+}
