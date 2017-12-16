906e304d41274acb9d7e58c4e85a194009459b9c
Sai Nithish <sainithishgvs@gmail.com>
2017-12-16 16:21:57 -0500

    input change

--- src/org/nithishgvs/arrays/AlternateHighLow.java
+++ src/org/nithishgvs/arrays/AlternateHighLow.java
@@ -9,8 +9,6 @@ public class AlternateHighLow {
 	 * @param array
 	 */
 	public static void arrangeAlternateHighLow(int[] array) {
-
-		Arrays.sort(array);
 		boolean flag = false;
 		for (int i = 0; i < array.length; i++) {
 			int j = i + 1;
@@ -40,9 +38,10 @@ public class AlternateHighLow {
 
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+		int[] array2 = { 9, 6, 8, 3, 7 };
 		System.out.println("Before Arranging");
-		System.out.println(Arrays.toString(array));
-		arrangeAlternateHighLow(array);
+		System.out.println(Arrays.toString(array2));
+		arrangeAlternateHighLow(array2);
 
 	}
 
