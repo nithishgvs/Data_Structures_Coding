101a999b9f2bea03742733901083d9308831db1a
Sai Nithish <sainithishgvs@gmail.com>
2018-10-10 20:27:06 -0400

    oct 10 commit

---
+++ src/org/leetcode/Trees/BinaryTreePaths_257.java
@@ -0,0 +1,61 @@
+package org.leetcode.Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class BinaryTreePaths_257 {
+	
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	List<String> output = new ArrayList<>();
+
+	public List<String> binaryTreePaths(TreeNode root) {
+		if (root == null)
+			return output;
+		if (root.left == null && root.right == null) {
+			output.add(String.valueOf(root.val));
+			return output;
+		}
+
+		treePaths(root.left, String.valueOf(root.val) + "->");
+		treePaths(root.right, String.valueOf(root.val) + "->");
+		return output;
+	}
+
+	private void treePaths(TreeNode node, String valueOf) {
+		if (node == null) {
+			return;
+		}
+		if (node.left == null && node.right == null) {
+			output.add(valueOf + String.valueOf(node.val));
+			return;
+		}
+		if (node != null) {
+			treePaths(node.left, valueOf + String.valueOf(node.val) + "->");
+			treePaths(node.right, valueOf + String.valueOf(node.val) + "->");
+		}
+		return;
+	}
+
+	@Test
+	public void testPaths() {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		// root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		// root.left.left.left = new TreeNode(6);
+		// root.left.left.right = new TreeNode(7);
+		binaryTreePaths(root);
+	}
+}
