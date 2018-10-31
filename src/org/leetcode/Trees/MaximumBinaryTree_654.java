2cd3257f2b9853cd8b59731374525ef2b8f8baab
Sai Nithish <sainithishgvs@gmail.com>
2018-10-31 19:16:19 -0400

    max binary tree`

--- src/org/leetcode/Trees/MaximumBinaryTree_654.java
+++ src/org/leetcode/Trees/MaximumBinaryTree_654.java
@@ -1,8 +1,40 @@
 package org.leetcode.Trees;
 
 import org.junit.Test;
+import sun.reflect.generics.tree.Tree;
 
 public class MaximumBinaryTree_654 {
+
+  public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+
+  public TreeNode constructMaximumBinaryTree(int[] nums) {
+    return maxTreeHelper(nums, 0, nums.length - 1);
+  }
+
+
+  TreeNode maxTreeHelper(int[] nums, int l, int h) {
+    if (l > h) {
+      return null;
+    }
+    if (l == h) {
+      new TreeNode(nums[l]);
+    }
+    int maxIndex = maxElementIndex(nums, l, h);
+    TreeNode root = new TreeNode(nums[maxIndex]);
+    root.left = maxTreeHelper(nums, l, maxIndex - 1);
+    root.right = maxTreeHelper(nums, maxIndex + 1, h);
+    return root;
+  }
 
 
   public int maxElementIndex(int[] array, int l, int h) {
@@ -22,8 +54,19 @@ public class MaximumBinaryTree_654 {
 
   @Test
   public void testTree() {
-    int[] array = {1, 6, 0, 1, 2, 16};
-    System.out.println(maxElementIndex(array, 0, array.length - 1));
+    int[] array = {3, 2, 1, 6, 0, 5};
+    TreeNode root = constructMaximumBinaryTree(array);
+    inOrderRecursive(root);
+  }
+
+  public void inOrderRecursive(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    inOrderRecursive(root.left);
+    System.out.print(root.val + " ");
+    inOrderRecursive(root.right);
+
   }
 
 }
